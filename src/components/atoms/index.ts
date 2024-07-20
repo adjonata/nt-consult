925f5976f0be859860211008f62e26d2db243818
adjonata <djonataalex@gmail.com>
2024-07-19 22:44:38 -0300

    feat: style adjustments + select component

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -3,3 +3,4 @@ export { default as Field } from "./field.vue";
 export { default as Modal } from "./modal.vue";
 export { default as Button } from "./button.vue";
 export { default as Label } from "./label.vue";
+export { default as Select } from "./select.vue";
---
+++ src/components/atoms/select.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import VueSelect from "vue-select";
+import Label from "./label.vue";
+import type { Option } from "@/types";
+
+const props = withDefaults(
+  defineProps<{
+    label: string;
+    name: string;
+    options: Option[];
+    placeholder?: string;
+    disabled?: boolean;
+  }>(),
+  {}
+);
+
+const value = defineModel();
+</script>
+
+<template>
+  <Label :label :for="name">
+    <VueSelect
+      v-model="value"
+      :name
+      :options
+      :placeholder
+      :disabled
+      :reduce="(option: Option) => option.code"
+      data-test="select"
+    />
+  </Label>
+</template>
+
+<style lang="scss">
+.vs__dropdown-toggle {
+  @apply h-[44px] pb-0 border border-[rgb(221,221,221)] focus:border-[rgb(170,174,183)] hover:border-[rgb(170,174,183)];
+}
+.vs__selected {
+  @apply h-[44px] my-0;
+}
+</style>
